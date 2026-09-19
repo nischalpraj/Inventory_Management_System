c8e32b5dee1db2aeb8f53815654a94285d4afca6
nischalpraj <nischalpraj776@gmail.com>
2026-09-19 15:57:18 +0545

    Created method signatures

--- src/main/java/org/example/dao/ProductDAOInterface.java
+++ src/main/java/org/example/dao/ProductDAOInterface.java
@@ -1,5 +1,24 @@
 package org.example.dao;
 
-public class ProductDAOInterface {
+import org.example.exception.ProductNotFoundException;
+import org.example.model.Product;
+
+import java.util.List;
+
+public interface ProductDAOInterface {
+
+    void addProduct(Product product);
+
+    List<Product> getAllProduct();
+
+    Product getProductById(int id) throws ProductNotFoundException;
+
+    Product getProductBySku(String sku) throws ProductNotFoundException;
+
+    boolean updateProduct(Product product);
+
+    boolean deleteProduct(int id);
+
+    boolean updateQuantity(int id,int newQuantity);
 
 }
