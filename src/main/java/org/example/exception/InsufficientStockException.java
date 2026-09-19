9b0481e9425c8b099432afbf0f9413eab1df9464
nischalpraj <nischalpraj776@gmail.com>
2026-09-19 15:31:59 +0545

    Made InsufficientStockException

---
+++ src/main/java/org/example/exception/InsufficientStockException.java
@@ -0,0 +1,12 @@
+package org.example.exception;
+
+public class InsufficientStockException extends Exception {
+
+    public InsufficientStockException(String message){
+        super(message);
+    }
+
+    public InsufficientStockException(String message,Throwable cause){
+        super(message,cause);
+    }
+}
