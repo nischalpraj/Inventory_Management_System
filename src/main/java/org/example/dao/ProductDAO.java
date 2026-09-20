a4cc7214ae242658212d7682aa81d79480b5ceb7
nischalpraj <nischalpraj776@gmail.com>
2026-09-20 15:07:09 +0545

    Made changes to fix the errors

--- src/main/java/org/example/dao/ProductDAO.java
+++ src/main/java/org/example/dao/ProductDAO.java
@@ -1,7 +1,91 @@
 package org.example.dao;
 
+import org.example.model.Electronics;
+import org.example.model.Grocery;
+import org.example.model.Product;
+import org.example.util.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductDAO implements ProductDAOInterface {
 
+    @Override
+    public void addProduct(Product product) {
+        String sql = "INSERT into products(sku,name,quantity,price,category,warranty_months,expiry_date)values(?,?,?,?,?,?,?)";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, product.getSku());
+            stmt.setString(2, product.getName());
+            stmt.setInt(3, product.getQuantity());
+            stmt.setDouble(4, product.getPrice());
+            stmt.setString(5, product.getCategory());
+
+            if (product instanceof Electronics) {
+                Electronics electronics = (Electronics) product;
+                stmt.setInt(6, electronics.getWarranty_months());
+                stmt.setNull(7, Types.DATE);
+            } else if (product instanceof Grocery) {
+                Grocery grocery = (Grocery) product;
+                stmt.setNull(6, Types.INTEGER);
+                stmt.setInt(7, grocery.getExpiry_date());
+            }
+
+            stmt.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Failde to add product" + e.getMessage(), e);
+        }
+    }
+
+        @Override
+                public List<Product> getAllProduct(){
+                String sql="Select * from products";
+                List <Product> products=new ArrayList<>();
+
+                try(Connection conn=DBConnection.getConnection();
+                PreparedStatement stmt=conn.prepareStatement(sql)){
+
+                    ResultSet rs= stmt.executeQuery();
+
+                    while (rs.next()){
+                        products.add(mapRowToProduct(rs));
+                }
+        } catch (SQLException e) {
+                    throw new RuntimeException("Failed to fetch product: "+e.getMessage(),e);
+
+                }
+                return products;
+    }
+
+
+
+
+    private Product mapRowToProduct(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String sku = rs.getString("sku");
+        String name = rs.getString("name");
+        int quantity = rs.getInt("quantity");
+        double price = rs.getDouble("price");
+        String category = rs.getString("category");
+
+        if ("Electronics".equalsIgnoreCase(category)) {
+            int warrantyMonths = rs.getInt("warranty_months");
+            return new Electronics(id, sku, name, quantity, price, warrantyMonths);
+
+        } else if ("Grocery".equalsIgnoreCase(category)) {
+            java.sql.Date expiry = rs.getDate("expiry_date");
+            String expiry_date = (expiry != null) ? expiry.toLocalDate().toString() : null;
+            return new Grocery(id, sku, name, quantity, price, expiry_date);
+
+        } else {
+            throw new IllegalStateException("Unknown product category in database: " + category);
+        }
+    }
 
 
 }
--- src/main/java/org/example/model/Grocery.java
+++ src/main/java/org/example/model/Grocery.java
@@ -2,19 +2,19 @@ package org.example.model;
 
 public class Grocery extends Product{
 
-    private int expiry_date;
+    private String expiry_date;
 
-    public Grocery(int id, String sku,String name, int quantity, double price,int expiry_date){
+    public Grocery(int id, String sku,String name, int quantity, double price,String expiry_date){
         super(id,sku,name,quantity,price);
         this.expiry_date=expiry_date;
     }
 
-    public Grocery(String sku,String name,int quanity,double price,int expiry_date){
+    public Grocery(String sku,String name,int quanity,double price,String expiry_date){
         super(sku,name,quanity,price);
         this.expiry_date=expiry_date;
     }
 
-    public int getExpiry_date(){
+    public String getExpiry_date(){
         return expiry_date;
     }
 
